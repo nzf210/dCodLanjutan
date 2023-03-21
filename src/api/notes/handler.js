8ae310ff64c61a079981e2e67fcc97e90ec440a1
Syahril Syamsudin <11546537-nzf210@users.noreply.gitlab.com>
2023-03-21 16:02:05 +0800

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -15,11 +15,11 @@ class NotesHandler {
         this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
     }
 
-    postNoteHandler(request, h) {
+    async postNoteHandler(request, h) {
         try {
-            this._validator.validateNotePayload(request.payload);
+            await this._validator.validateNotePayload(request.payload);
             const { title = 'untitled', body, tags } = request.payload;
-            const noteId = this._service.addNote({ title, body, tags });
+            const noteId = await this._service.addNote({ title, body, tags });
             const response = h.response({
                 status: 'success',
                 message: 'Catatan berhasil ditambahkan',
@@ -54,8 +54,8 @@ class NotesHandler {
             return response;
         }
     }
-    getNotesHandler() {
-        const notes = this._service.getNotes();
+    async getNotesHandler() {
+        const notes = await this._service.getNotes();
         return {
             status: 'success',
             data: {
@@ -63,10 +63,10 @@ class NotesHandler {
             },
         };
     }
-    getNoteByIdHandler(request, h) {
+    async getNoteByIdHandler(request, h) {
         try {
             const { id } = request.params;
-            const note = this._service.getNoteById(id);
+            const note = await this._service.getNoteById(id);
             return {
                 status: 'success',
                 data: {
@@ -96,16 +96,14 @@ class NotesHandler {
             response.code(500);
             console.error(error);
             return response;
-
-
         }
     }
-    putNoteByIdHandler(request, h) {
+    async putNoteByIdHandler(request, h) {
         try {
-            this._validator.validateNotePayload(request.payload);
+            await this._validator.validateNotePayload(request.payload);
             const { id } = request.params;
 
-            this._service.editNoteById(id, request.payload);
+            await this._service.editNoteById(id, request.payload);
 
             return {
                 status: 'success',
@@ -137,10 +135,10 @@ class NotesHandler {
             return response;
         }
     }
-    deleteNoteByIdHandler(request, h) {
+    async deleteNoteByIdHandler(request, h) {
         try {
             const { id } = request.params;
-            this._service.deleteNoteById(id);
+            await this._service.deleteNoteById(id);
             return {
                 status: 'success',
                 message: 'Catatan berhasil dihapus',
